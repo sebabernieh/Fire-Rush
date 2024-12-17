050963fcde4929d12c362a7b868647c6dc223f6b
lanaperu <lanaaldroubi123@gmail.com>
2024-12-17 17:15:48 +0300

    Add files via upload

---
+++ Assets/Scripts/AudioManger.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManger : MonoBehaviour
+{
+    public static AudioManger instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public AudioSource[] music;
+    public AudioSource[] sfx;
+
+    public void PlayMusic(int trackNumber)
+    {
+        StopMusic();
+
+        if (trackNumber < music.Length)
+        {
+            music[trackNumber].Play();
+        }
+    }
+
+    public void StopMusic()
+    {
+        foreach (AudioSource track in music)
+        {
+            track.Stop();
+        }
+    }
+}
